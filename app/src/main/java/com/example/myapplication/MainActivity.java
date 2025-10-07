119332079022781fddfbc07359863fa6dd06efb6
adeboraguedes <deboraguedesetec@gmail.com>
2025-10-07 20:46:43 -0300

    Arrumando o desing

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nome_var;
+    EditText curso_var;
+    Button botao_var;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
